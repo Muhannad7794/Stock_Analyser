d85e324391458100421ef06a008bbb137c5fa0f4
Muhannad7794 <mual0001@stud.kea.dk>
2024-03-12 15:16:20 +0000

    added the smtp variables to the docker compose file

--- stock_analyser/settings.py
+++ stock_analyser/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 # backend email settings
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
